774dc7a883db124e315ba11b47665733d97cf5d4
Michael Webb <62156343+michaelwebbj@users.noreply.github.com>
2021-03-14 18:27:46 -0600

    Add files via upload

---
+++ fuel/app/views/maintemplate.php
@@ -0,0 +1,23 @@
+<head>
+	<meta charset="utf-8"></meta>
+	<meta name="author" content="Michael Webb"></meta>
+	<meta name="description" content="Fuel Template for M1"></meta>
+
+</head>
+
+<body>
+    <header>
+        <ul>
+            <li><a href="https://cs.colostate.edu:4444/~mwebbj/m1/index/">Home</a></li>
+            <li><a href="https://cs.colostate.edu:4444/~mwebbj/m1/index/about">About</a></li>
+            <li><a href="https://cs.colostate.edu:4444/~mwebbj/m1/index/ccg">Color Coordinate Generation</a></li>
+        </ul>
+    </header>
+    <main>  
+
+    </main>
+    <footer>
+	<br>
+       	Created by Michael Webb, Jake Paschke, Eileen Rice for CS312
+    </footer>
+</body>
