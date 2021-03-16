9c51373aa1a8bac801b1e5a65c514ae5bce82314
Michael Webb <62156343+michaelwebbj@users.noreply.github.com>
2021-03-15 23:21:42 -0600

    Update main.php

--- fuel/app/classes/controller/main.php
+++ fuel/app/classes/controller/main.php
@@ -1,15 +1,20 @@
 <?php
 class Controller_Main extends Controller_Template{
-
+	
 	public $template = 'maintemplate';
 
 	public function action_index()
 	{
+		$this->template->title= 'Home Page';
         	$this->template->content=View::forge('m1/index');
+		$this->template->css = "main.css";
+		//return Response::forge(View::forge('m1/index'));
 	}
+
 	public function action_about()
 	{
-		$this->template->content=View::forge('m1/index/about');
+		$this->template->title= 'About Page';
+        	$this->template->content=View::forge('m1/index/about');
+		$this->template->css = "main.css";
 	}
 }
-
