e5901592df236bebb330f7f82a465e29d7dc73a3
Michael Webb <62156343+michaelwebbj@users.noreply.github.com>
2021-03-15 23:17:42 -0600

    Update maintemplate.php

--- fuel/app/views/maintemplate.php
+++ fuel/app/views/maintemplate.php
@@ -1,20 +1,22 @@
 <head>
+	<title><?php echo$title; ?> </title>
 	<meta charset="utf-8"></meta>
 	<meta name="author" content="Michael Webb"></meta>
 	<meta name="description" content="Fuel Template for M1"></meta>
-
+	<?php echo Asset::css($css) ?>
 </head>
 
 <body>
     <header>
+	<h1><?php echo$title; ?></h1>
         <ul>
-            <li><a href="https://cs.colostate.edu:4444/~mwebbj/m1/index/">Home</a></li>
+            <li><a href="https://cs.colostate.edu:4444/~mwebbj/m1/index/main">Home</a></li>
             <li><a href="https://cs.colostate.edu:4444/~mwebbj/m1/index/about">About</a></li>
             <li><a href="https://cs.colostate.edu:4444/~mwebbj/m1/index/ccg">Color Coordinate Generation</a></li>
         </ul>
     </header>
     <main>  
-
+	<?php echo $content ?>
     </main>
     <footer>
 	<br>
