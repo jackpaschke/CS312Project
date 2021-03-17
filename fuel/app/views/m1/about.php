a72e03a21da254762bd614c9799a80e7d65d5ced
Eileen Rice <46908247+eileenrice1@users.noreply.github.com>
2021-03-17 17:05:48 -0600

    Update about.php

--- fuel/app/views/m1/about.php
+++ fuel/app/views/m1/about.php
@@ -1,18 +1,32 @@
-<header>
-    <h1> About Page <h1>
-</header>
 <body>
     <main>
         <div class="row">
             <div class="column">
-                <h2>Michael Webb</h2>
+                <div class="card">
+                    <div class="container">
+                        <img src="" alt="Michael Webb">
+                        <h2>Michael Webb</h2>
+                        <p>Biography</p>
+                    </div>
+                </div>
             </div>
-            
             <div class="column">
-                <h2>Jake Paschke/h2>
+                <div class="card">
+                    <div class="container">
+                        <img src="" alt="Jake Paschke">
+                        <h2>Jake Paschke</h2>
+                        <p>Biography</p>
+                    </div>
+                </div>
             </div>
             <div class="column">
-                <h2>Eileen Rice/h2>
+                <div class="card">
+                    <div class="container">
+                        <img src="" alt="Eileen Rice">
+                        <h2>Eileen Rice</h2>
+                        <p>Biography</p>
+                    </div>
+                </div>
             </div>
         </div>
     </main>
