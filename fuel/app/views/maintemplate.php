e5292f91e6265ea7516179828a8e8beb03369928
Eileen Rice <46908247+eileenrice1@users.noreply.github.com>
2021-03-17 17:08:29 -0600

    Update maintemplate.php

--- fuel/app/views/maintemplate.php
+++ fuel/app/views/maintemplate.php
@@ -8,18 +8,29 @@
 
 <body>
     <header>
-	<h1><?php echo$title; ?></h1>
-        <ul>
-            <li><a href="https://cs.colostate.edu:4444/~mwebbj/m1/index/main">Home</a></li>
-            <li><a href="https://cs.colostate.edu:4444/~mwebbj/m1/index/main/about">About</a></li>
-            <li><a href="https://cs.colostate.edu:4444/~mwebbj/m1/index/main/ccg">Color Coordinate Generation</a></li>
-        </ul>
+        <h1> 
+            <img src="https://cs.colostate.edu:4444/~eileenr/m1/img/nemotech.png" width="190"  title="Logo of a company" alt="Logo of a company" />
+            <?php echo$title; ?> 
+        </h1>
     </header>
+    <nav>
+        <ul>
+            <li><a href="https://cs.colostate.edu:4444/~eileenr/m1/index/main">Home</a></li>
+            <li><a href="https://cs.colostate.edu:4444/~eileenr/m1/index/main/about">About</a></li>
+            <li><a href="https://cs.colostate.edu:4444/~eileenr/m1/index/main/ccg">Color Coordinate Generation</a></li>
+        </ul>
+    </nav>
     <main>  
 	    <?php echo $content ?>
     </main>
     <footer>
 	<br>
        	Created by Michael Webb, Jake Paschke, Eileen Rice for CS312
+        <p>Copyright © 2020 Nemo Technologies</p>
+            <p>
+                <a>About</a> -
+                <a>Privacy Policy</a> -
+                <a>Contact Us</a>
+            </p>
     </footer>
 </body>
