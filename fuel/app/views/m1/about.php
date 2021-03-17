77ec3ccf98ece7cf83242685e523db760e9c84ed
eileenrice1 <eileenr@rams.colostate.edu>
2021-03-17 13:19:41 -0600

    Create about css and update about page

--- fuel/app/views/m1/about.php
+++ fuel/app/views/m1/about.php
@@ -1,1 +1,19 @@
-
+<header>
+    <h1> About Page <h1>
+</header>
+<body>
+    <main>
+        <div class="row">
+            <div class="column">
+                <h2>Michael Webb</h2>
+            </div>
+            
+            <div class="column">
+                <h2>Jake Paschke/h2>
+            </div>
+            <div class="column">
+                <h2>Eileen Rice/h2>
+            </div>
+        </div>
+    </main>
+</body>
