7cbb81b04c4708b135fcad7239476ad9cecf2bbf
Jack Paschke <jack.paschke@gmail.com>
2021-03-24 16:48:57 -0600

    added bio

--- fuel/app/views/m1/about.php
+++ fuel/app/views/m1/about.php
@@ -15,7 +15,8 @@
                     <div class="container">
                     <?php echo Asset::img('jack.jpeg', array('style' => 'height: 300px')) ?>
                         <h2>Jake Paschke</h2>
-                        <p>Biography</p>
+                        <h3>Biography</h3>
+                        <p>Jack is a senior in computer science concentrating in human centered computing. He is interested in accessibility and human factors in computing. </p>
                     </div>
                 </div>
             </div>
