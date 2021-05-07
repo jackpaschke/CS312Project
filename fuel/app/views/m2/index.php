3d66069f61ba79a350cd779c64dfd1d3e1eaccc8
Eileen Rice <46908247+eileenrice1@users.noreply.github.com>
2021-05-07 16:10:03 -0600

    Menbers -> members

--- fuel/app/views/m2/index.php
+++ fuel/app/views/m2/index.php
@@ -2,7 +2,7 @@
 	<main>
 		
 		<h2>
-		Welcome to Nemo Tech! Here you can read more about our team menbers and access our web applications.
+		Welcome to Nemo Tech! Here you can read more about our team members and access our web applications.
 </h2>
 	
 	</main>
